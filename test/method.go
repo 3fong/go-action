f051794e12011a8c08482663b2c865027ca6e5ab
liulei <liulei1@365trade.com.cn>
2020-08-12 21:32:19 +0800

    add: 添加方法

---
+++ test/method.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Point 结构体
+type Point struct{ X, Y float64 }
+
+// Distance function
+func Distance(p, q Point) float64 {
+    return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// Distance but as a method of the Point type
+func (p Point) Distance(q Point) float64 {
+    return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+type I int
+
+func (i I) Sum(q I) int {
+    return int(i) + int(q)
+}
+
+func main(){
+	printDistance()
+}
+
+func printDistance() {
+	p := Point{1, 2}
+	q := Point{4, 6}
+	fmt.Println(Distance(p, q)) // "5", function call
+	fmt.Println(p.Distance(q))  // "5", method call
+}
