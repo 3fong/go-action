f7729c61a97565479ea7e49858e8895e96b960dc
liulei <liulei1@365trade.com.cn>
2020-08-24 18:53:43 +0800

    update: 增加接口值内容

--- test/interface.go
+++ test/interface.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"fmt"
+	"io"
+	"os"
 	"time"
 )
-
 
 type user interface {
 	login() string
@@ -25,32 +28,51 @@ func (f *file) login2() string {
 	return "len(p)"
 }
 func (f file) Read(p []byte) (n int, err error) {
-	fmt.Println(p,f.name,f.date)
-	return len(p),nil
+	fmt.Println(p, f.name, f.date)
+	return len(p), nil
 }
 func (f file) Write(p []byte) (n int, err error) {
-	fmt.Println(p,f.name,f.date)
-	return len(p),nil
+	fmt.Println(p, f.name, f.date)
+	return len(p), nil
 }
 
-func main(){
-	f := file{name:"liu",date:time.Now()}
-	fmt.Println(f.login2)
+func main() {
+	// f := file{name:"liu",date:time.Now()}
+	// fmt.Println(f.login2)
 	// var p = []byte{1,1}
 	// f.Read(p)
-	
-	var db interface{}
-	fmt.Println(db)
+	compare()
 }
 
-type IntSet struct { X string }
+type IntSet struct{ X string }
+
 func (i *IntSet) String() string {
 	return i.X
 }
 
+var period = flag.Duration("period", 1*time.Second, "sleep period")
 
-type db interface {}
+func receiveParam() {
+	flag.Parse()
+	fmt.Printf("Sleeping for %v...", *period)
+	time.Sleep(*period)
+	fmt.Println()
+}
 
-func (d db) aa() string {
-	return "aa"
+func compare() {
+	var w io.Writer
+	fmt.Printf("%T\n", w)
+	var i *io.Writer
+	fmt.Printf("%T\n", i)
+	w = os.Stdout
+	fmt.Printf("%T\n", w)
+	w = new(bytes.Buffer)
+	fmt.Printf("%T\n", w)
+	w = nil
+	fmt.Printf("%T\n", w)
+	var a interface{} = 1
+	fmt.Println(a == a)
+
+	var x interface{} = []int{1, 2, 3}
+	fmt.Println(x == x)
 }
