baa519588615254d256362183d8980ab98cb4861
liulei <liulei1@365trade.com.cn>
2020-09-06 23:11:01 +0800

    add: 增加channel示例

---
+++ test/routine/concurrent/countdown2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	// ...create abort channel...
+
+	//!-
+
+	//!+abort
+	abort := make(chan struct{})
+	go func() {
+		os.Stdin.Read(make([]byte, 1)) // read a single byte
+		abort <- struct{}{}
+	}()
+	//!-abort
+
+	//!+
+	fmt.Println("Commencing countdown.  Press return to abort.")
+	select {
+	case t := <-time.After(10 * time.Second):
+		fmt.Println(t)
+	case <-abort:
+		fmt.Println("Launch aborted!")
+		return
+	}
+	launch()
+}
+
+func launch() {
+	fmt.Println("Lift off!")
+}
--- test/routine/thumbnails.go
+++ test/routine/thumbnails.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"gopl.io/ch8/thumbnail"
@@ -21,6 +22,43 @@ func makeThumbnails2(filenames []string) {
 }
 
 func main(){
-	files := []string{"test/routine/file/2.jpg"}
-	makeThumbnails2(files)
+	files := []string{"test/routine/file/3.png","test/routine/file/4.png"}
+	fmt.Println(makeThumbnails4(files)) 
+}
+
+func makeThumbnails3(filenames []string) {
+    ch := make(chan struct{})
+    for _, f := range filenames {
+        go func(f string) {
+            thumbnail.ImageFile(f) // NOTE: ignoring errors
+            ch <- struct{}{}
+        }(f)
+    }
+    // Wait for goroutines to complete.
+    for range filenames {
+        <-ch
+    }
+}
+
+
+func makeThumbnails4(filenames []string) error {
+    errors := make(chan error)
+
+    for _, f := range filenames {
+        go func(f string) {
+            _, err := thumbnail.ImageFile(f)
+			errors <- err
+			fmt.Println(err)
+			fmt.Println(len(errors))
+        }(f)
+    }
+
+    for range filenames {
+        if err := <-errors; err != nil {
+			fmt.Println(len(errors))
+            return err // NOTE: incorrect: goroutine leak!
+        }
+    }
+
+    return nil
 }
