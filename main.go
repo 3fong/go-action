984685017c03d80caac685c36ac187891ebabb36
liulei <liulei1@365trade.com.cn>
2020-11-30 10:25:37 +0800

    aaa

--- main.go
+++ main.go
@@ -3,19 +3,14 @@ package main
 import (
 	"fmt"
 	"runtime"
-	"time"
 
-	"github.com/liulei3/go-action/test/myreflect/format"
+	"github.com/liulei3/go-action/test/myreflect"
 	"github.com/liulei3/go-action/test/routine/change"
 )
 
 func main() {
-	var x int64 = 1
-	var d time.Duration = 1 * time.Nanosecond
-	fmt.Println(format.Any(x))          // "1"
-	fmt.Println(format.Any(d))          // "1"
-	fmt.Println(format.Any([]int64{x})) // "[]int64 0x8202b87b0"
-	fmt.Println(format.Any([]time.Duration{d}))
+	myreflect.PrintType()
+	myreflect.Print(myreflect.GetObj())
 }
 
 func PrintData() {
--- test/myreflect/format_test.go
+++ test/myreflect/format_test.go
@@ -4,15 +4,13 @@ import (
 	"fmt"
 	"testing"
 	"time"
-
-	"github.com/liulei3/go-action/test/myreflect/format"
 )
 
 func TestAny(t *testing.T) {
 	var x int64 = 1
 	var d time.Duration = 1 * time.Nanosecond
-	fmt.Println(format.Any(x))                  // "1"
-	fmt.Println(format.Any(d))                  // "1"
-	fmt.Println(format.Any([]int64{x}))         // "[]int64 0x8202b87b0"
-	fmt.Println(format.Any([]time.Duration{d})) // "[]time.Duration 0x8202b87e0"
+	fmt.Println(Any(x))                  // "1"
+	fmt.Println(Any(d))                  // "1"
+	fmt.Println(Any([]int64{x}))         // "[]int64 0x8202b87b0"
+	fmt.Println(Any([]time.Duration{d})) // "[]time.Duration 0x8202b87e0"
 }
